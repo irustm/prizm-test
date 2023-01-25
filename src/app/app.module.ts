8076b6458543a1c91ca1a08dcaf377fc228b84a8
Rustam <jamakrus@gmail.com>
2023-01-25 20:14:22 +0300

    up: chore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
+import { PrizmUiRootModule } from "@prizm-ui/components";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { PrizmButtonModule } from '@prizm-ui/components';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,8 +9,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+      BrowserModule,
+      PrizmUiRootModule,
+      PrizmButtonModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
